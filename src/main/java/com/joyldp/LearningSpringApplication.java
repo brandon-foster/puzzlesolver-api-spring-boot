78e1fe2f7ee5810165c868ec410de61976c7b786
Brandon <brandon.foster.vt@gmail.com>
2021-08-12 21:25:16 -0400

    Make /puzzle-is-solvable endpoint.

--- src/main/java/com/joyldp/LearningSpringApplication.java
+++ src/main/java/com/joyldp/LearningSpringApplication.java
@@ -19,6 +19,13 @@ public class LearningSpringApplication {
 		SpringApplication.run(LearningSpringApplication.class, args);
 	}
 
+	public class IsSolvable {
+		private Boolean isSolvable;
+		public IsSolvable(Boolean b) { this.isSolvable = b; }
+		public Boolean getIsSolvable() { return isSolvable; }
+		public void setIsSolvable(Boolean b) { this.isSolvable = b; }
+	}
+
 	public class StepPath {
 		private String path;
 		public StepPath(String path) {
@@ -29,6 +36,52 @@ public class LearningSpringApplication {
 		}
 		public void setPath(String path) {
 			this.path = path;
+		}
+	}
+
+	@RestController
+	@RequestMapping("/puzzle-is-solvable")
+	public class PuzzleIsSolvableController {
+		@GetMapping
+		public Boolean isSolvable(@RequestParam(value="initial-state", required = true)String initialState) {
+			String outputFlag = null;
+			String queueFlag = null;
+			String verbosityFlag = null;
+			String[] stateArray = new String[19];
+			if (initialState != null) {
+				// separate the values by commas
+				String[] stringArrayFromInitialState = initialState.split(",");
+				for (int i = 0; i < 16; i++)
+				{
+					stateArray[i] = stringArrayFromInitialState[i + 3];
+				}
+				outputFlag = stringArrayFromInitialState[0];
+				queueFlag = stringArrayFromInitialState[1];
+				verbosityFlag = stringArrayFromInitialState[2];
+			}
+			// create the initial PuzzleState
+			PuzzleState puzzleState = new PuzzleState(stateArray);
+			// create the PuzzleSolver (the graph)
+			PuzzleSolver solver = new PuzzleSolver();
+			solver.init(puzzleState, outputFlag, queueFlag, verbosityFlag);
+			solver.start();
+			try {
+				solver.join(4000);
+			} catch (InterruptedException e) {
+				System.out.println("Request handler caught InterruptedException: " + e);
+			}
+			Boolean isAlive = solver.isAlive();
+			System.out.println("solver.getState(): " + solver.getState());
+			System.out.println("solver.isAlive(): " + isAlive);
+			if (isAlive) {
+				System.out.println("Interrupting solver...");
+				solver.interrupt();
+			}
+			System.out.println(solver.getState());
+			System.out.println("solver.isAlive(): " + solver.isAlive());
+			System.out.println("solver.isInterrupted(): " + solver.isInterrupted());
+			System.out.println(solver.getState());
+			return !isAlive;
 		}
 	}
 
@@ -69,13 +122,17 @@ public class LearningSpringApplication {
 			// create the PuzzleSolver (the graph)
 			PuzzleSolver puzzleSolver = new PuzzleSolver();
 			// initialize the PuzzleSolver with the initial PuzzleState
-			puzzleSolver.init(outputFlag, queueFlag, verbosityFlag);
+			puzzleSolver.init(puzzleState, outputFlag, queueFlag, verbosityFlag);
 			// prepare for capturing output of PuzzleSolver
 			ByteArrayOutputStream newOut = new ByteArrayOutputStream();
 			PrintStream originalOut = System.out;
 			System.setOut(new PrintStream(newOut));
 			// run the breadthFirstSearch() with the initial puzzle state
-			puzzleSolver.breadthFirstSearch(puzzleState);
+			try {
+				puzzleSolver.breadthFirstSearch(puzzleState);
+			} catch (InterruptedException e) {
+				System.out.println("Request handler /puzzlesolver caught InterruptedException: " + e);
+			}
 			// set output back to standard output
 			System.setOut(originalOut);
 			String result = newOut.toString();
